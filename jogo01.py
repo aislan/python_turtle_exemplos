3061a3c19b2541ac9dbce2ef3e3b1ac53ab1040f
aislan rafael <aislanmaster@gmail.com>
2019-07-26 09:54:05 -0300

    jogo parte 2

--- jogo01.py
+++ jogo01.py
@@ -10,9 +10,27 @@ jogador = Turtle()
 jogador.color('blue')
 jogador.shape('triangle')
 jogador.penup()
+jogador.speed(0)
 
 #velocidade
 velocidade = 1
+
+#ações dos comandos
+def vira_a_esquerda():
+    jogador.left(30)
+
+def vira_a_direita():
+    jogador.right(30)
+
+def aumenta_velocidade():
+    global velocidade
+    velocidade += 1
+
+#comandos do jogo
+listen()
+onkey(vira_a_esquerda,"Left")
+onkey(vira_a_direita,"Right")
+onkey(aumenta_velocidade,"Up")
 
 #principal
 while True:
